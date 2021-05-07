7bbf5003643d2709089a252ad77974fe6b598f99
OrtizAlex <46968556+OrtizAlex@users.noreply.github.com>
2021-05-07 18:40:28 -0400

    added search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,40 @@
 var apiKey = "768d8906dffdb8442fb5b977a6c0b4ff";
 
+var searchHistory;
+if(JSON.parse(localStorage.getItem("history")) != null)
+    searchHistory = JSON.parse(localStorage.getItem("history"));
+else    
+    searchHistory = [];
+
 var searchBtn = document.getElementById("search-button");
-searchBtn.addEventListener("click", searchResults)
+searchBtn.addEventListener("click", searchResults);
 
-function searchResults(){
+searchList();
 
-    if(document.getElementById("search").value !== null){
-        
-        var search = document.getElementById("search").value;
-        document.getElementById("search").value = "";
-        console.log(search);
+function searchResults() {
+    var search = document.getElementById("search").value;
+    if (document.getElementById("search").value !== "") {
+
         weatherSearch(search);
         forecastSearch(search);
-       
+
+        saveSearch(search);
+        renderSearch();
+
+        document.getElementById("search").value = "";
     }
 }
 
-
-function weatherSearch(search){
+function weatherSearch(search) {
     var todayHeader = document.getElementById("today-header");
     todayHeader.className = "";
 
     var api = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`;
     fetch(api)
-        .then(function (response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function (data){
+        .then(function (data) {
             var todayEl = document.getElementById("today");
             todayEl.textContent = "";
 
@@ -49,27 +57,27 @@ function weatherSearch(search){
 
             var temperature = document.createElement("p");
             temperature.className = "card-text";
-            temperature.textContent = "Temperature: " + data.main.temp +  "°F";
+            temperature.textContent = "Temperature: " + data.main.temp + "°F";
 
             var uvIndex = document.createElement('p');
             uvIndex.className = "card-text";
 
             fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${data.coord.lat}&lon=${data.coord.lon}`)
-                .then(function (response){
+                .then(function (response) {
                     return response.json();
                 })
-                .then(function (data){
+                .then(function (data) {
                     uvIndex.textContent = "UV Index: " + data.value;
-                    if(data.value < 7)
+                    if (data.value < 7)
                         uvIndex.className = "text-warning";
-                    if(data.value < 3)
+                    if (data.value < 3)
                         uvIndex.className = "text-primary";
                     else
                         uvIndex.className = "text-danger";
                 });
 
             var imgEl = document.createElement("img");
-            imgEl.setAttribute("src",`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+            imgEl.setAttribute("src", `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
 
             cardHeader.appendChild(imgEl);
             card.appendChild(cardHeader);
@@ -79,61 +87,89 @@ function weatherSearch(search){
             card.appendChild(uvIndex);
             cardContainer.appendChild(card);
             todayEl.appendChild(cardContainer);
-        })   
+        });
 }
 
-function forecastSearch(search){
+function forecastSearch(search) {
     var forecastHeader = document.getElementById("forecast-header");
     forecastHeader.className = "";
 
     var api = `http://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${apiKey}`;
 
     fetch(api)
-        .then(function (response){
+        .then(function (response) {
             return response.json()
         })
-        .then(function (data){
+        .then(function (data) {
             var forecastEl = document.getElementById("forecast");
 
             for (var i = 0; i < data.list.length; i++) {
                 if (data.list[i].dt_txt.includes("12:00:00")) {
-                  var cards = document.createElement('div');
+                    var cards = document.createElement('div');
 
-                  var cardContainer = document.createElement('div');
-                  cardContainer.className = "card bg-primary text-white";
+                    var cardContainer = document.createElement('div');
+                    cardContainer.className = "card bg-primary text-white";
 
-                  var card = document.createElement('div');
-                  card.className = "card-body p-2";
+                    var card = document.createElement('div');
+                    card.className = "card-body p-2";
 
-                  var cardHeader = document.createElement('h3');
-                  cardHeader.className = "card-title";
-                  cardHeader.textContent = data.list[i].dt_txt.split("12:")[0];
+                    var cardHeader = document.createElement('h3');
+                    cardHeader.className = "card-title";
+                    cardHeader.textContent = data.list[i].dt_txt.split("12:")[0];
 
-                  var wind = document.createElement('p');
-                  wind.className = "card-text";
-                  wind.textContent = "Wind Speed: " + data.list[i].wind.speed + "MPH";
+                    var wind = document.createElement('p');
+                    wind.className = "card-text";
+                    wind.textContent = "Wind Speed: " + data.list[i].wind.speed + "MPH";
 
-                  var humidity = document.createElement('p');
-                  humidity.className = "card-text"
-                  humidity.textContent = "Humidity : " + data.list[i].main.humidity + "%";
+                    var humidity = document.createElement('p');
+                    humidity.className = "card-text"
+                    humidity.textContent = "Humidity : " + data.list[i].main.humidity + "%";
 
-                  var temperature = document.createElement('p');
-                  temperature.className = "card-text"
-                  temperature.textContent = "Temperature : " + data.list[i].main.temp_max + "°F";
+                    var temperature = document.createElement('p');
+                    temperature.className = "card-text"
+                    temperature.textContent = "Temperature : " + data.list[i].main.temp_max + "°F";
 
-                  var icon = document.createElement('img');
-                  icon.setAttribute('src', `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`);
-      
-                  card.appendChild(cardHeader);
-                  card.appendChild(icon);
-                  card.appendChild(wind);
-                  card.appendChild(humidity);
-                  card.appendChild(temperature);
-                  cardContainer.appendChild(card);
-                  cards.appendChild(cardContainer);
-                  forecastEl.appendChild(cardContainer);
+                    var icon = document.createElement('img');
+                    icon.setAttribute('src', `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`);
+
+                    card.appendChild(cardHeader);
+                    card.appendChild(icon);
+                    card.appendChild(wind);
+                    card.appendChild(humidity);
+                    card.appendChild(temperature);
+                    cardContainer.appendChild(card);
+                    cards.appendChild(cardContainer);
+                    forecastEl.appendChild(cardContainer);
                 }
-              }
+            }
         });
 
+}
+
+function saveSearch(search){
+    if(!searchHistory.includes(search)){
+        searchHistory.push(search);
+        localStorage.setItem("history", JSON.stringify(searchHistory));
+    }
+}
+
+function renderSearch(){
+    while (document.getElementById("history").firstChild) {
+        document.getElementById("history").removeChild(document.getElementById("history").firstChild);
+    }
+    searchList();
+}
+
+function searchList(){
+    searchHistory.forEach(function (search){
+        var historyItem = document.createElement("li");
+        historyItem.className = "list-group-item";
+        historyItem.textContent = search;
+ 
+        historyItem.addEventListener("click", function(event){
+            weatherSearch(event.target.textContent);
+            forecastSearch(event.target.textContent);
+        });
+        document.getElementById("history").appendChild(historyItem);
+    });
 }
